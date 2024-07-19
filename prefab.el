0d8968f3b6cd6879f983f884db22cefb66210d6c
Alan Pitts <alanmpitts@gmail.com>
2024-07-19 14:13:34 -0400

    I forgot to add env to py code find_templates()

--- prefab.el
+++ prefab.el
@@ -192,7 +192,7 @@ import os.path
 ctx = %s
 envvars = ctx.get('cookiecutter', {}).get('_jinja2_env_vars', {})
 env = StrictEnvironment(context=ctx, keep_trailing_newline=True, **envvars)
-template_dir = find_template('%s')
+template_dir = find_template('%s', env)
 dirname = os.path.split(template_dir)[1]
 output_dir = '%s'
 
